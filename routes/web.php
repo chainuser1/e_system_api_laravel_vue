d9a9ce3cc6e8ea42547a50e65f5d6b0e8b8b6fb2
Jayson Rael Ramos <lothlorien654@gmail.com>
2022-06-16 09:32:17 +0800

    Add Images

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,5 @@ Route::view('/{path?}', 'spa')->where('path', '^(?!api|register|login|home).*$')
 // Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
+
+// routes to load pictures
